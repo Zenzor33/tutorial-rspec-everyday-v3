9eb73235379a68fbb613f4934eb425c60a6cba43
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2024-07-07 17:31:39 +0200

    controller specs complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,13 @@ class TasksController < ApplicationController
 
   # GET /tasks/1
   # GET /tasks/1.json
+
   def show
+    # Can delete this block when finished with controller specs?
+    respond_to do |format|
+      format.html
+      format.json { render json: @task }
+    end
   end
 
   # GET /tasks/new
@@ -31,7 +37,7 @@ class TasksController < ApplicationController
     respond_to do |format|
       if @task.save
         format.html { redirect_to @project, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: [@task.project, @task] }
+        format.json { render json: @task, status: :created, location: [@task.project, @task] }
       else
         format.html { render :new }
         format.json { render json: @task.errors, status: :unprocessable_entity }
--- app/models/project.rb
+++ app/models/project.rb
@@ -15,7 +15,7 @@ class Project < ApplicationRecord
 
   belongs_to :owner, class_name: 'User', foreign_key: :user_id
   has_many :notes
-  # has_many :tasks
+  has_many :tasks
 
   def late?
     due_on.in_time_zone < Date.current.in_time_zone
