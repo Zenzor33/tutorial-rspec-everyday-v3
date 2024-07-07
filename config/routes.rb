f2f632581096f3220455c1bdf97474447592783e
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2024-07-07 17:09:02 +0200

    tasks imported

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :projects
+  resources :projects do 
+    resources :tasks do 
+      member do 
+        post :toggle
+      end
+    end
+  end 
   # Defines the root path route ("/")
   root "home#index"
 end
