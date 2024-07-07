a95b338f8e51b615fa6958a3b0aa37a79d37c6f7
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2024-07-07 08:29:05 +0200

    passing specs for projects_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :projects
   # Defines the root path route ("/")
   root "home#index"
 end
