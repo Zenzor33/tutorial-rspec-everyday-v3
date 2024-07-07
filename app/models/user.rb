eeaa4de6c945ec428438829d400901b6d3382391
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2024-07-07 08:09:42 +0200

    devise installed

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,9 @@
 #
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
